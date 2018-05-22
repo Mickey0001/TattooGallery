98b561ee6d324a078960a03ee5146af4cdfb9da9
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-22 13:15:52 -0400

    Images Gallery

--- tattoo/src/Components/Gallery/Gallery.js
+++ tattoo/src/Components/Gallery/Gallery.js
@@ -7,6 +7,36 @@ import Five from '../../../src/Images/5.jpg';
 import Six from '../../../src/Images/6.jpg';
 import Seven from '../../../src/Images/7.jpg';
 import Eight from '../../../src/Images/8.jpg';
+import Nine from '../../../src/Images/9.jpg';
+import Ten from '../../../src/Images/10.jpg';
+import Twelwe from '../../../src/Images/12.jpg';
+import Thirteen from '../../../src/Images/13.jpg';
+import Fourteen from '../../../src/Images/14.jpg';
+import Fifteen from '../../../src/Images/15.jpg';
+import Sixteen from '../../../src/Images/16.jpg';
+import Seventeen from '../../../src/Images/17.jpg';
+import Eighteen from '../../../src/Images/18.jpg';
+import Nineteen from '../../../src/Images/19.jpg';
+import Twenty from '../../../src/Images/20.jpg';
+import Twentyone from '../../../src/Images/21.jpg';
+import Twentytwo from '../../../src/Images/22.jpg';
+import Twentythree from '../../../src/Images/23.jpg';
+import Twentyfour from '../../../src/Images/24.jpg';
+import Twentyfive from '../../../src/Images/25.jpg';
+import Twentysix from '../../../src/Images/26.jpg';
+import Twentyseven from '../../../src/Images/27.jpg';
+import Twentyeight from '../../../src/Images/28.jpg';
+import Twentynine from '../../../src/Images/29.jpg';
+import Thirty from '../../../src/Images/30.jpg';
+import Thirtyone from '../../../src/Images/31.jpg';
+import Thirtytwo from '../../../src/Images/32.jpg';
+import Thirtythree from '../../../src/Images/33.jpg';
+import Thirtyfour from '../../../src/Images/34.jpg';
+import Thirtyfive from '../../../src/Images/35.jpg';
+import Thirtysix from '../../../src/Images/36.jpg';
+import Thirtyseven from '../../../src/Images/37.jpg';
+import Thirtyeight from '../../../src/Images/38.jpg';
+import Thirtynine from '../../../src/Images/39.jpg';
 
 const Gallery = () => (
   <div className="row">
@@ -61,49 +91,115 @@ const Gallery = () => (
 
        <div class="col-lg-3 col-md-4 col-xs-6">
       <a href="#" class="d-block mb-4 h-100">
-        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""/>
+        <img class="img-fluid img-thumbnail" src={Nine} alt=""/>
       </a>
     </div>
 
        <div class="col-lg-3 col-md-4 col-xs-6">
       <a href="#" class="d-block mb-4 h-100">
-        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""/>
+        <img class="img-fluid img-thumbnail" src={Ten} alt=""/>
       </a>
     </div>
 
        <div class="col-lg-3 col-md-4 col-xs-6">
       <a href="#" class="d-block mb-4 h-100">
-        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""/>
+        <img class="img-fluid img-thumbnail" src={Twelwe} alt=""/>
       </a>
     </div>
 
        <div class="col-lg-3 col-md-4 col-xs-6">
       <a href="#" class="d-block mb-4 h-100">
-        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""/>
+        <img class="img-fluid img-thumbnail" src={Thirteen} alt=""/>
       </a>
     </div>
 
        <div class="col-lg-3 col-md-4 col-xs-6">
       <a href="#" class="d-block mb-4 h-100">
-        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""/>
+        <img class="img-fluid img-thumbnail" src={Fifteen} alt=""/>
       </a>
     </div>
 
        <div class="col-lg-3 col-md-4 col-xs-6">
       <a href="#" class="d-block mb-4 h-100">
-        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""/>
+        <img class="img-fluid img-thumbnail" src={Sixteen} alt=""/>
       </a>
     </div>
 
-       <div class="col-lg-3 col-md-4 col-xs-6">
+    <div class="col-lg-3 col-md-4 col-xs-6">
       <a href="#" class="d-block mb-4 h-100">
-        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""/>
+        <img class="img-fluid img-thumbnail" src={Seventeen} alt=""/>
       </a>
     </div>
 
-       <div class="col-lg-3 col-md-4 col-xs-6">
+        <div class="col-lg-3 col-md-4 col-xs-6">
       <a href="#" class="d-block mb-4 h-100">
-        <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt=""/>
+        <img class="img-fluid img-thumbnail" src={Eighteen} alt=""/>
+      </a>
+    </div>
+
+            <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Nineteen} alt=""/>
+      </a>
+    </div>
+
+            <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Twenty} alt=""/>
+      </a>
+    </div>
+
+                <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Twentyone} alt=""/>
+      </a>
+    </div>
+
+            <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Twentytwo} alt=""/>
+      </a>
+    </div>
+
+                 <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Twentythree} alt=""/>
+      </a>
+    </div>
+
+            <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Twentyfour} alt=""/>
+      </a>
+    </div>
+
+                 <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Twentyfive} alt=""/>
+      </a>
+    </div>
+
+            <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Twentysix} alt=""/>
+      </a>
+    </div>
+    
+    <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Twentyseven} alt=""/>
+      </a>
+    </div>
+
+                 <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Twentyeight} alt=""/>
+      </a>
+    </div>
+
+            <div class="col-lg-3 col-md-4 col-xs-6">
+      <a href="#" class="d-block mb-4 h-100">
+        <img class="img-fluid img-thumbnail" src={Twentynine} alt=""/>
       </a>
     </div>
 
