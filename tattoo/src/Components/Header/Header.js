0fd35490b4edb89c0746ee34d05208c79b830f5d
Mirza Sisic <mirzasisic57@gmail.com>
2018-05-23 12:15:59 -0400

    CSS and image corrections

--- tattoo/src/Components/Header/Header.js
+++ tattoo/src/Components/Header/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Cover from '../../../src/Images/cover.jpg';
 
 const Header = () => (
-      <div className="row">
+      <div className="container">
           <img src={Cover} className="img-fluid" alt="Tattoo Studio Akademija Logo"/>
-          <div className="text-center">
+          <div className="row">
             <h1>MITAR ĐINOVIĆ</h1><br/>
-            <h3 className="text-center">Akademski umetnik sa master studijama i profesionalni tattoo majstor. </h3><br/>
+            <h3 >Akademski umetnik sa master studijama i profesionalni tattoo majstor. </h3><br/>
           </div><br/>
       </div>
   );
